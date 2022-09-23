48ff5fe9d3503be338cbe014faf70eced49af1c0
kylesloper <kylesloper@gmail.com>
2022-09-23 15:38:54 +0100

    :construction: fixing Gotrue is not a constructor

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,7 +1,4 @@
 import GotTrue from 'gotrue-js'
 import { readable } from 'svelte/store'
 
-const url = 'https://bespoke-haupia-b4041c.netlify.app'
-export const session = readable(
-  new GotTrue({ setCookie: true, APIUrl: `${url}/.netlify/identity` })
-)
+export const session = readable(new GotTrue({ setCookie: true }))
